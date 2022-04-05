fa5794fb7d73d8f08f7c26c0bea87b0915e4961e
Faust VIII <joh.faustviii@gmail.com>
2022-04-05 22:14:54 +0200

    Add custom Prometheus gauge about read bytes in last second

--- src/main/java/org/faust/stats/StatsService.java
+++ src/main/java/org/faust/stats/StatsService.java
@@ -1,19 +1,32 @@
 package org.faust.stats;
 
+import io.micrometer.core.instrument.Metrics;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit;
+import java.util.stream.Collectors;
 
+@EnableScheduling
 @Service
 public class StatsService {
 
     private final List<ForwardingStats> stats = new ArrayList<>();
+    private final List<ForwardingStats> prometheusStats;
 
     public StatsService() {
+        prometheusStats = Metrics.gauge("readBytes", Collections.emptyList(), new ArrayList<>(),
+                list -> list
+                        .stream()
+                        .filter(stat -> System.currentTimeMillis() - stat.getTimestamp() < 1000)
+                        .mapToInt(ForwardingStats::getCount)
+                        .sum());
         ScheduledExecutorService executorService = Executors.newScheduledThreadPool(1);
         executorService.scheduleAtFixedRate(this::show, 5, 5, TimeUnit.SECONDS);
     }
@@ -21,6 +34,7 @@ public class StatsService {
     public void add(ForwardingStats forwardingStats) {
         synchronized (stats) {
             stats.add(forwardingStats);
+            prometheusStats.add(forwardingStats);
         }
     }
 
@@ -31,6 +45,17 @@ public class StatsService {
                 System.out.println("ThreadId: " + stat.getThreadId() + "\t timestamp: " + stat.getTimestamp() + "\t read: " + stat.getCount());
             }
             stats.clear();
+        }
+    }
+
+    @Scheduled(fixedRate = 1000)
+    public void removePrometheusStats() {
+        List<ForwardingStats> statsToRemove = prometheusStats.stream()
+                .filter(stat -> System.currentTimeMillis() - stat.getTimestamp() > 1000)
+                .collect(Collectors.toList());
+        prometheusStats.removeAll(statsToRemove);
+        if (statsToRemove.size() > 0) {
+            System.out.println("Removed " + statsToRemove.size() + " Prometheus stats");
         }
     }
 }
