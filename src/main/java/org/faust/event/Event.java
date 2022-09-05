2357fd945fde91615fa6b8dfad65fba047f157b9
Faust VIII <joh.faustviii@gmail.com>
2022-09-05 21:56:24 +0200

    WIP: Add Lombok

--- src/main/java/org/faust/event/Event.java
+++ src/main/java/org/faust/event/Event.java
@@ -1,47 +1,25 @@
 package org.faust.event;
 
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
-@Entity(name = "events") // TODO: lombok
+@Data
+@NoArgsConstructor
+@RequiredArgsConstructor
+@Entity(name = "events")
 public class Event {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id; // TODO: change to UUID?
     private Long timestamp = System.currentTimeMillis();
+    @NonNull
     private String message;
-
-    public Event() {
-    }
-
-    Event(String message) {
-        this.message = message;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public Long getTimestamp() {
-        return timestamp;
-    }
-
-    public void setTimestamp(Long timestamp) {
-        this.timestamp = timestamp;
-    }
-
-    public String getMessage() {
-        return message;
-    }
-
-    public void setMessage(String message) {
-        this.message = message;
-    }
 }
