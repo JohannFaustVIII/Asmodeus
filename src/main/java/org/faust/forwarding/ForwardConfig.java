5526927cbe62f3afd9fc6e6592da1561661b8fa8
Faust VIII <joh.faustviii@gmail.com>
2022-09-20 22:02:43 +0200

    Use Lombok in ForwardConfig

--- src/main/java/org/faust/forwarding/ForwardConfig.java
+++ src/main/java/org/faust/forwarding/ForwardConfig.java
@@ -1,5 +1,12 @@
 package org.faust.forwarding;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
 public class ForwardConfig {
 
     private int inputPort;
@@ -13,42 +20,6 @@ public class ForwardConfig {
     private int packetAge;
 
     private String forwardName;
-
-    public ForwardConfig() {
-    }
-
-    public ForwardConfig(int inputPort, int outputPort, String outputIp, int packetsCount, int packetAge, String forwardName) {
-        this.inputPort = inputPort;
-        this.outputPort = outputPort;
-        this.outputIp = outputIp;
-        this.packetsCount = packetsCount;
-        this.packetAge = packetAge;
-        this.forwardName = forwardName;
-    }
-
-    public int getInputPort() {
-        return inputPort;
-    }
-
-    public int getOutputPort() {
-        return outputPort;
-    }
-
-    public String getOutputIp() {
-        return outputIp;
-    }
-
-    public int getPacketsCount() {
-        return packetsCount;
-    }
-
-    public int getPacketAge() {
-        return packetAge;
-    }
-
-    public String getForwardName() {
-        return forwardName;
-    }
 
     @Override
     public String toString() {
